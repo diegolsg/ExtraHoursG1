03dab7269644578cfd92e5eda73c86ed478970cf
Maria <julianahenao1510@gmail.com>
2025-04-03 10:08:19 -0500

    Cambio en UserDto

--- ExtraHours.Core/dto/UserDto.cs
+++ ExtraHours.Core/dto/UserDto.cs
@@ -2,11 +2,10 @@
 {
     public class UserDto
     {
-        public required string Name { get; set; }
-        public required string PhoneNumber { get; set; }
-        public required string Password { get; set; }
-        public required string Email { get; set; }
-        // public int AreaId { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
         public int RoleId { get; set; }
     }
 }
