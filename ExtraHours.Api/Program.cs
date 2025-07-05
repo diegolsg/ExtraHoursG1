5293c9caa5512bc52c2995013a0a3c6fb964582e
Maria <julianahenao1510@gmail.com>
2025-07-04 20:38:39 -0500

    Elimino configuracion

--- ExtraHours.Api/Program.cs
+++ ExtraHours.Api/Program.cs
@@ -92,11 +92,4 @@ app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
-
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
-}
-
 app.Run();
